39a8d3af88b9e6aa042fa0e3e45f804d61150a70
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-20 16:44:12 -0300

    Remove test code from my_list class file.

--- my_list.rb
+++ my_list.rb
@@ -11,17 +11,3 @@ class MyList
     @list.each(&block)
   end
 end
-
-# Create our list
-list = MyList.new(1, 2, 3, 4)
-
-# Test #all?
-puts(list.all? { |e| e < 5 }) # Output: true
-puts(list.all? { |e| e > 5 }) # Output: false
-
-# Test #any?
-puts(list.any? { |e| e == 2 }) # Output: true
-puts(list.any? { |e| e == 5 }) # Output: false
-
-# Test #filter
-puts(list.filter(&:even?)) # Output: [2, 4]
